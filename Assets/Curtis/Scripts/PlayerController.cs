c4b662e5e6d2e1e01609f6a5cc24708712d9898c
MUKILTEO\1717482 <1717482@Mukilteo.wednet.edu>
2025-09-17 09:33:22 -0700

    Character movement.

---
+++ Assets/Curtis/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Data;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private float horizontal;
+
+    private CharacterController Cc;
+
+    public float jumpHeight = 5f;
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask WhatIsGround;
+
+    private bool isGrounded;
+
+    public bool isFacingRight = true;
+
+
+    void Start()
+    {
+        Cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, WhatIsGround);
+
+        horizontal = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        Vector3 move = new Vector3(0, 0, horizontal);
+
+        Cc.Move(move * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        Cc.Move(velocity * Time.deltaTime);
+
+        if (horizontal > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+
+        else if (horizontal < 0 && isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.z *= -1;
+        transform.localScale = localScale;
+    }
+}
