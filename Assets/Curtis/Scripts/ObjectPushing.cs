86a26d341275e28462ba6824f4426fe88ff75df9
MUKILTEO\1717482 <1717482@Mukilteo.wednet.edu>
2025-09-18 08:58:52 -0700

    Pushing cubes n' stuff

---
+++ Assets/Curtis/Scripts/ObjectPushing.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ObjectPushing : MonoBehaviour
+{
+    public float forceMagnitude;
+
+    Rigidbody rb;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        rb = hit.collider.attachedRigidbody;
+
+        if (rb != null)
+        {
+            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+            forceDirection.y = 0;
+            forceDirection.Normalize();
+
+            rb.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+        }
+    }
+}
